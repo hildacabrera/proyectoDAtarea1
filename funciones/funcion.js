a19900e0c0d97f707e0e4f7dccbaf971f882163f
hildacabrera <hilda.cabrera@est.iudigital.edu.co>
2023-03-05 22:22:34 -0500

    Entrega de taller de interfaz de usuario

---
+++ funciones/funcion.js
@@ -0,0 +1,9 @@
+
+/* Mostrar el menu mostrar */
+function mostrar(){
+    document.getElementById('sliding_menu').classList.add('open')
+}
+/* Ocultar el menu */
+function ocultar(){
+    document.getElementById('sliding_menu').classList.remove('open')
+}
